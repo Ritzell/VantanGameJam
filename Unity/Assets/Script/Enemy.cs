4ab037208c99c4dab848f3bd0d5226fc403e549b
河島有志 <kawasimayuji@gmail.com>
2016-10-01 14:13:16 +0900

    EnemyとBulletのプログラムを実装。

--- Unity/Assets/Script/Enemy.cs
+++ Unity/Assets/Script/Enemy.cs
@@ -2,14 +2,27 @@
 using System.Collections;
 
 public class Enemy : MonoBehaviour {
-
+	[SerializeField]
+	private float AttackFrequency = 1;
+	[SerializeField]
+	private GameObject InstanceWeapon;
 	// Use this for initialization
-	void Start () {
-	
+	void Start() {
+		StartCoroutine(Attack());
 	}
-	
+
+	IEnumerator Attack()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(AttackFrequency);
+			Instantiate(InstanceWeapon,transform.position,transform.rotation);
+			yield return null;
+		}
+	}
+
 	// Update is called once per frame
 	void Update () {
-	
+
 	}
 }
---
+++ Unity/Assets/Script/bullet.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bullet : MonoBehaviour {
+	void Update () {
+		transform.LookAt(FindObjectOfType<Player>().transform);
+		transform.Translate(0,0,5*Time.deltaTime);
+	}
+}
