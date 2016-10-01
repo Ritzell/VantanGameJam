f17a5f5e5b6f50c5e262b3894032a38ec628b907
河島有志 <kawasimayuji@gmail.com>
2016-10-01 17:55:09 +0900

    アイテム獲得メソッドを作り直し。時計を設置。

--- Unity/Assets/Script/Player.cs
+++ Unity/Assets/Script/Player.cs
@@ -10,6 +10,8 @@ public class Player : MonoBehaviour {
 	private SceneMove SceneMoveScript;
 	[SerializeField]
 	private float MaxSpeed = 10;
+	[SerializeField]
+	private float SpeedUpTime = 5;
 	private bool isOnFloor = true;
 	private Coroutine WalkingCoroutine;
 	private Vector3 screenPos;
@@ -73,6 +75,13 @@ public class Player : MonoBehaviour {
 			rigidbody.velocity = new Vector3(MaxSpeed * (Mathf.Abs (rigidbody.velocity.x) / rigidbody.velocity.x),rigidbody.velocity.y,rigidbody.velocity.z);
 		}
 	}
+
+	public IEnumerator SpeedUp(float Multiplication){
+		MaxSpeed = MaxSpeed * Multiplication;
+		yield return new WaitForSeconds (SpeedUpTime);
+		MaxSpeed = MaxSpeed / Multiplication;
+	}
+
 	private void CameraLockAtMe(float x){
 		//Debug.Log (x >= 0 && screenPos.x > Screen.width / 3);
 		if (x >= 0 && screenPos.x > Screen.width / 3) {//画面の左半分半ばに入ればカメラが付いていく。
